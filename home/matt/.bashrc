589693590d2bd4d257e30a3c6064567ad7b5675c
Matthew Woelk <matt.woelk@gmail.com>
2011-09-17 22:53:43 -0500

    super updates! the script is now ... betterthanever!

--- home/matt/.bashrc
+++ home/matt/.bashrc
@@ -10,6 +10,7 @@ alias ls='ls --color=auto'
 alias pacman=pacman-color
 alias vlc-for-remote='vlc -I http'
 alias bc='bc -q'
+alias info='info --vi-keys'
 
 #--PROMPT--
 #PS1='[\u@\h \W]\$ ' #The arch default
@@ -18,7 +19,6 @@ PS1="\[\033[0;32m\][\u@\h \w]$\[\033[0m\] "
 #Tab-Completion in sudo and man
 complete -cf sudo
 complete -cf man
-
 
 
 #--ENVIRONMENTAL VARIABLES--
--- home/matt/scripts/writepackageinfofile.sh
+++ home/matt/scripts/writepackageinfofile.sh
@@ -1,33 +1,47 @@
 #!/bin/bash
 
 PROGRAMLIST="/home/matt/programs.txt"
-PROGRAMDUMP="/home/matt/programdump.txt"
+PROGRAMDUMP="/home/matt/programdump.temp"
 
 yaourt -Qet | cut -d'/' -f2 | cut -d' ' -f1 > "$PROGRAMDUMP"
+# PROGRAMDUMP now contains a list of all installed programs 
+# and it will continue to do so for the duration of this script.
 
+# delete the last paragraph of PROGRAMLIST
+sed '/UNSORTED/ q' < "$PROGRAMLIST" > "$PROGRAMLIST".temp
+cp "$PROGRAMLIST".temp "$PROGRAMLIST"
+
+# get rid of everything from PROGRAMDUMP which is still in PROGRAMLIST
+# read, line-by-line from PROGRAMDUMP
 while read line; do
-	#echo "$line"
-	# see if the current line is in PROGRAMLIST
-	cat "$PROGRAMLIST" | grep "^$line" > /dev/null
-	if [ $? -ne 0 ]
-	then
-		echo "$line" >> "$PROGRAMLIST"
-	fi
+  #echo "$line"
+  # see if the current line is in PROGRAMLIST
+  cat "$PROGRAMLIST" | grep "^$line" > /dev/null
+  if [ $? -ne 0 ]
+  then
+    # if it wasn't already in the file somewhere, add it
+    # to PROGRAMLIST
+    echo "$line" >> "$PROGRAMLIST"
+  fi
 done < "$PROGRAMDUMP"
 
 # take the last paragraph of PROGRAMLIST and sort it
-PRESORTEDLIST=$(sed '1,/UNSORTED/ d' < "$PROGRAMLIST")
-SORTEDLIST=$(echo "$PRESORTEDLIST" | sort)
+# TODO: this may no longer me necessary...
+#PRESORTEDLIST=$(sed '1,/UNSORTED/ d' < "$PROGRAMLIST")
+#SORTEDLIST=$(echo "$PRESORTEDLIST" | sort)
+#echo "$SORTEDLIST" >> "$PROGRAMLIST"
 
-# delete the last paragraph of PROGRAMLIST TODO: FIX THIS
-sed '/UNSORTED/ q' < "$PROGRAMLIST" > "$PROGRAMLIST"2
-cp "$PROGRAMLIST"2 "$PROGRAMLIST"
-rm "$PROGRAMLIST"2
+# get rid of temp files
+rm "$PROGRAMLIST".temp
 rm "$PROGRAMDUMP"
 
-# add the new, sorted paragraph to PROGRAMLIST
-echo "$SORTEDLIST" >> "$PROGRAMLIST"
 
+# TODO TODO TODO TODO todo TODO TODO TODO TODO
+# new program flow:
+#   [done] delete "unsorted" paragraph from PROGRAMLIST
+#   [done] take out everything from PROGRAMDUMP which is still in PROGRAMLIST
+#   [done] add programdump to the end of PROGRAMLIST
+#   [x] profit
 
 #-----------------------------------------------#
 #sed '/UNSORTED/ q' will print every line until (and including) UNSORTED
