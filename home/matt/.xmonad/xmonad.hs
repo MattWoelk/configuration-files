b12f7c30ceeed1ac03e9480a292baf265a755b32
Matthew Woelk <matt.woelk@gmail.com>
2011-09-18 14:43:07 -0500

    casual. so casual. also, xmonad is getting usable now.

--- home/matt/.xmonad/xmonad.hs
+++ home/matt/.xmonad/xmonad.hs
@@ -1,9 +1,17 @@
 import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Accordion
+
+mylayoutHook = (avoidStruts $ Tall 1 (3/100) (488/792)) ||| noBorders (Full)
 
 main = do
   xmonad $ defaultConfig
     { terminal  = "urxvt -bg black -fg white +sb"
     , borderWidth = 2
-    , normalBorderColor   = "#111111"
+    , normalBorderColor   = "#080808"
     , focusedBorderColor  = "#00CC00"
+--    , layoutHook=avoidStruts $ mylayoutHook
+    , layoutHook=mylayoutHook
+    , manageHook=manageDocks <+> manageHook defaultConfig
     }
