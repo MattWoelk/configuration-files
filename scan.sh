5a313181db46f30b62508d404ced7c6da993a9e9
Matthew Woelk <matt.woelk@gmail.com>
2011-09-24 14:53:31 -0500

    Got rid of depreciated daemons blacklisting

--- scan.sh
+++ scan.sh
@@ -2,4 +2,5 @@
 # This takes all of the files in path-list.txt and makes a file structure
 # in the current directory and copies them into it.
 
+#                   v-- is this middle pipe'd part even necessary...?
 cat path-list.txt | xargs -I{} echo {} | cpio -pduL --quiet . 
