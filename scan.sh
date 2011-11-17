8c235b8e489bdde0836b961f1e91b9dfd5fac8e7
Matthew Woelk <matt.woelk@gmail.com>
2011-11-16 19:27:40 -0600

    added the ability for scan.sh to import entire directories that are specified

--- scan.sh
+++ scan.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 # This takes all of the files in path-list.txt and makes a file structure
 # in the current directory and copies them into it.
+# If a directory name is given in path-list.txt, the entire directory
+# and all subdirectories will be copied
 
 # to make this cronabble
 cd $(dirname $0)
 
-cat path-list.txt | cpio -pduL --quiet . 
-#cat path-list.txt | find | cpio -pduL --quiet . 
-#                     ^-- find should repeat any file name, and list all subitems of a directory
-# THIS WILL BREAK THE DIRECTORY CURRENTLY (not sure why...)
+while read line; do
+  find -L $line | cpio -pduL --quiet .
+done < path-list.txt
+
+
+# old method: (didn't do path names)
+#cat path-list.txt | cpio -pduL --quiet . 
 
 modprobe -c > modprobe_-c.txt
